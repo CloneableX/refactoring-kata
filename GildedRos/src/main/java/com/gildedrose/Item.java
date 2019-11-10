24844e631cf7aca7277cffa04d9603125db0723c
Cloneable <1573652017@qq.com>
2019-11-10 18:28:13 +0800

    move special operator of item to inner of item

--- GildedRos/src/main/java/com/gildedrose/Item.java
+++ GildedRos/src/main/java/com/gildedrose/Item.java
@@ -18,4 +18,71 @@ public class Item {
    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
+
+    boolean isSulfuras() {
+        return name.equals("Sulfuras, Hand of Ragnaros");
+    }
+
+    boolean isBackstagePasses() {
+        return name.equals("Backstage passes to a TAFKAL80ETC concert");
+    }
+
+    boolean isArgedBrie() {
+        return name.equals("Aged Brie");
+    }
+
+    void updateQualityAfterSellIn() {
+        if (sellIn < 0) {
+            if (!isArgedBrie()) {
+                if (!isBackstagePasses()) {
+                    if (quality > 0) {
+                        if (!isSulfuras()) {
+                            quality = quality - 1;
+                        }
+                    }
+                } else {
+                    quality = 0;
+                }
+            } else {
+                if (quality < 50) {
+                    quality = quality + 1;
+                }
+            }
+        }
+    }
+
+    void updateQuality() {
+        if (!isArgedBrie()
+                && !isBackstagePasses()) {
+            if (quality > 0) {
+                if (!isSulfuras()) {
+                    quality = quality - 1;
+                }
+            }
+        } else {
+            if (quality < 50) {
+                quality = quality + 1;
+
+                if (isBackstagePasses()) {
+                    if (sellIn < 11) {
+                        if (quality < 50) {
+                            quality = quality + 1;
+                        }
+                    }
+
+                    if (sellIn < 6) {
+                        if (quality < 50) {
+                            quality = quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    void updateSellIn() {
+        if (!isSulfuras()) {
+            sellIn = sellIn - 1;
+        }
+    }
 }
