8c5485d9ceea2cf7f5073ab8a6c378bcb633a6bb
Cloneable <1573652017@qq.com>
2019-11-11 22:35:05 +0800

    remove unused code

--- BugsZero/src/test/java/com/adaptionsoft/games/trivia/GameTest.java
+++ BugsZero/src/test/java/com/adaptionsoft/games/trivia/GameTest.java
@@ -1,19 +1,18 @@
 package com.adaptionsoft.games.trivia;
 
-import static org.junit.Assert.*;
-
 import com.adaptionsoft.games.trivia.runner.GameRunner;
 import org.approvaltests.Approvals;
 import org.junit.Test;
 
-import java.io.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Random;
 import java.util.stream.IntStream;
 
 public class GameTest {
 
 	@Test
-	public void itsLockedDown() throws Exception {
+	public void itsLockedDown() {
 
         Random randomizer = new Random(123455);
         ByteArrayOutputStream resultStream = new ByteArrayOutputStream();
