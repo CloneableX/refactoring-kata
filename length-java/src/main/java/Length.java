c07bbf6ef96d077114d71bcadd2e31fc402f7222
Cloneable <1573652017@qq.com>
2020-01-18 22:31:07 +0800

    extract constants of u

--- length-java/src/main/java/Length.java
+++ length-java/src/main/java/Length.java
@@ -1,4 +1,7 @@
 public class Length {
+    public static final String YARD = "yard";
+    public static final String INCH = "inch";
+    public static final String F = "f";
     private final double value;
     private final String unit;
 
@@ -9,26 +12,26 @@ public class Length {
 
     public Length as(String u) {
         Length len = this;
-        if (this.unit.equals("f")) {
-            if (u.equals("yard")) {
+        if (this.unit.equals(F)) {
+            if (u.equals(YARD)) {
                 len = new Length(this.value / 3, u);
-            } else if (u.equals("inch")) {
+            } else if (u.equals(INCH)) {
                 len = new Length(this.value * 12, u);
             }
         }
 
-        if (this.unit.equals("yard")) {
-            if (u.equals("inch")) {
+        if (this.unit.equals(YARD)) {
+            if (u.equals(INCH)) {
                 len = new Length(this.value * 36, u);
-            } else if (u.equals("f")){
+            } else if (u.equals(F)){
                 len = new Length(this.value * 3, u);
             }
         }
 
-        if (this.unit.equals("inch")) {
-            if (u.equals("f")) {
+        if (this.unit.equals(INCH)) {
+            if (u.equals(F)) {
                 len = new Length(this.value / 12, u);
-            } else if (u.equals("yard")) {
+            } else if (u.equals(YARD)) {
                 len = new Length(this.value / 36, u);
             }
         }
