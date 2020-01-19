c5c5bde461055ee766a9581317e3385d98202b25
Cloneable <1573652017@qq.com>
2020-01-19 16:02:32 +0800

    move convert of Inch to Yard.java

---
+++ length-java/src/main/java/Inch.java
@@ -0,0 +1,18 @@
+public class Inch extends Unit {
+    public Inch() {
+        super(UNIT_INCH);
+    }
+
+    @Override
+    public Length convert(String tarUnit, double value) {
+        if (unitEquals(tarUnit, UNIT_F)) {
+            return new Length(value / 12, tarUnit);
+        }
+
+        if (unitEquals(tarUnit, UNIT_YARD)) {
+            return new Length(value / 36, tarUnit);
+        }
+
+        return super.convert(tarUnit, value);
+    }
+}
--- length-java/src/main/java/Unit.java
+++ length-java/src/main/java/Unit.java
@@ -8,10 +8,6 @@ public class Unit {
         this.type = type;
     }
 
-    protected Unit() {
-
-    }
-
     static boolean unitEquals(String srcUnit, String tarUnit) {
         return srcUnit.equals(tarUnit);
     }
@@ -19,6 +15,10 @@ public class Unit {
     public static Unit create(String type) {
         if (UNIT_YARD.equals(type))
             return new Yard();
+
+        if (UNIT_INCH.equals(type))
+            return new Inch();
+
         return new Unit(type);
     }
 
@@ -33,14 +33,6 @@ public class Unit {
 
         if (checkConversionWay(UNIT_F, tarUnit, UNIT_INCH)) {
             return new Length(value * 12, tarUnit);
-        }
-
-        if (checkConversionWay(UNIT_INCH, tarUnit, UNIT_F)) {
-            return new Length(value / 12, tarUnit);
-        }
-
-        if (checkConversionWay(UNIT_INCH, tarUnit, UNIT_YARD)) {
-            return new Length(value / 36, tarUnit);
         }
 
         return null;
