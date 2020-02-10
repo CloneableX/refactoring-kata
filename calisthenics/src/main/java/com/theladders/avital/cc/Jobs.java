41c604963914c67da8647fb4d068202ef0ceb732
Cloneable <1573652017@qq.com>
2020-02-10 16:46:42 +0800

    modify field jobsMap value to List<Job>

--- calisthenics/src/main/java/com/theladders/avital/cc/Jobs.java
+++ calisthenics/src/main/java/com/theladders/avital/cc/Jobs.java
@@ -4,22 +4,26 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Jobs {
-    private Map<String, List<List<String>>> jobsMap = new HashMap<>();
+    private Map<String, List<Job>> jobsMap = new HashMap<>();
 
     public void saveJob(Employer employer, Job job) {
-        List<List<String>> jobs = jobsMap.getOrDefault(employer.getName(), new ArrayList<>());
+        List<Job> jobs = jobsMap.getOrDefault(employer.getName(), new ArrayList<>());
 
-        jobs.add(new ArrayList<>() {{
-            add(job.getName());
-            add(job.getTypeName());
-        }});
+        jobs.add(job);
         jobsMap.put(employer.getName(), jobs);
     }
 
     public List<List<String>> getJobs(String employerName) {
-        return jobsMap.get(employerName);
+        List<Job> jobs = jobsMap.get(employerName);
+        return jobs.stream()
+                .map(job -> new ArrayList<String>() {{
+                    add(job.getName());
+                    add(job.getTypeName());
+                }})
+                .collect(Collectors.toList());
     }
 
     public void publishJob(Employer employer, Job job) throws EmptyJobTypeException {
