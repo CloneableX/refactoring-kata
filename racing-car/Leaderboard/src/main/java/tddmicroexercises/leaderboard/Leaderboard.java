5e871ef1f2ff3178627cba4239843255baabe29c
Cloneable <1573652017@qq.com>
2020-04-09 21:43:11 +0800

    extract method calculateDriversPoint

--- racing-car/Leaderboard/src/main/java/tddmicroexercises/leaderboard/Leaderboard.java
+++ racing-car/Leaderboard/src/main/java/tddmicroexercises/leaderboard/Leaderboard.java
@@ -12,14 +12,17 @@ public class Leaderboard {
 
     public Map<String, Integer> driverResults() {
         Map<String, Integer> results = new HashMap<>();
+        calculateDriversPoint().forEach(driver -> results.put(driver.getName(), driver.getPoint()));
+        return results;
+    }
+
+    private Set<Driver> calculateDriversPoint() {
         Set<Driver> drivers = new HashSet<>();
         for (Race race : this.races) {
             race.calculateDiversPoint();
             drivers.addAll(race.getResults());
         }
-
-        drivers.forEach(driver -> results.put(driver.getName(), driver.getPoint()));
-        return results;
+        return drivers;
     }
 
     public List<String> driverRankings() {
