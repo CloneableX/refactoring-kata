0507f268a7ac84447868dcf4857ac74703b0cd7e
Cloneable <1573652017@qq.com>
2020-04-14 22:10:00 +0800

    extract status message report to TelemetrySimulator.simulateStatusMessage

--- racing-car/TelemetrySystem/src/main/java/tddmicroexercises/telemetrysystem/TelemetryClient.java
+++ racing-car/TelemetrySystem/src/main/java/tddmicroexercises/telemetrysystem/TelemetryClient.java
@@ -17,8 +17,6 @@ public class TelemetryClient {
             throw new IllegalArgumentException();
         }
 
-        // simulate the operation on a real modem
-
         onlineStatus = telemetrySimulator.simulateOnline();
     }
 
@@ -33,23 +31,7 @@ public class TelemetryClient {
         }
 
         if (message.equals(DIAGNOSTIC_MESSAGE)) {
-            // simulate a status report
-            diagnosticMessageResult =
-                    "LAST TX rate................ 100 MBPS\r\n"
-                            + "HIGHEST TX rate............. 100 MBPS\r\n"
-                            + "LAST RX rate................ 100 MBPS\r\n"
-                            + "HIGHEST RX rate............. 100 MBPS\r\n"
-                            + "BIT RATE.................... 100000000\r\n"
-                            + "WORD LEN.................... 16\r\n"
-                            + "WORD/FRAME.................. 511\r\n"
-                            + "BITS/FRAME.................. 8192\r\n"
-                            + "MODULATION TYPE............. PCM/FM\r\n"
-                            + "TX Digital Los.............. 0.75\r\n"
-                            + "RX Digital Los.............. 0.10\r\n"
-                            + "BEP Test.................... -5\r\n"
-                            + "Local Rtrn Count............ 00\r\n"
-                            + "Remote Rtrn Count........... 00";
-
+            diagnosticMessageResult = telemetrySimulator.simulateStatusMessage();
         }
 
         // here should go the real Send operation (not needed for this exercise)
--- racing-car/TelemetrySystem/src/main/java/tddmicroexercises/telemetrysystem/TelemetrySimulator.java
+++ racing-car/TelemetrySystem/src/main/java/tddmicroexercises/telemetrysystem/TelemetrySimulator.java
@@ -22,4 +22,21 @@ public class TelemetrySimulator {
     public boolean simulateOnline() {
         return simulator.nextInt(10) <= 8;
     }
+
+    public String simulateStatusMessage() {
+        return "LAST TX rate................ 100 MBPS\r\n"
+                + "HIGHEST TX rate............. 100 MBPS\r\n"
+                + "LAST RX rate................ 100 MBPS\r\n"
+                + "HIGHEST RX rate............. 100 MBPS\r\n"
+                + "BIT RATE.................... 100000000\r\n"
+                + "WORD LEN.................... 16\r\n"
+                + "WORD/FRAME.................. 511\r\n"
+                + "BITS/FRAME.................. 8192\r\n"
+                + "MODULATION TYPE............. PCM/FM\r\n"
+                + "TX Digital Los.............. 0.75\r\n"
+                + "RX Digital Los.............. 0.10\r\n"
+                + "BEP Test.................... -5\r\n"
+                + "Local Rtrn Count............ 00\r\n"
+                + "Remote Rtrn Count........... 00";
+    }
 }
