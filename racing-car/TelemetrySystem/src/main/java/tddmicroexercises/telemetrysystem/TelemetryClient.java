ee63647a432aa2989e489591811c65bbf91ff54a
Cloneable <1573652017@qq.com>
2020-04-14 21:53:11 +0800

    extract method simulateMessage

--- racing-car/TelemetrySystem/src/main/java/tddmicroexercises/telemetrysystem/TelemetryClient.java
+++ racing-car/TelemetrySystem/src/main/java/tddmicroexercises/telemetrysystem/TelemetryClient.java
@@ -37,19 +37,19 @@ public class TelemetryClient {
             // simulate a status report
             diagnosticMessageResult =
                     "LAST TX rate................ 100 MBPS\r\n"
-                    + "HIGHEST TX rate............. 100 MBPS\r\n"
-                    + "LAST RX rate................ 100 MBPS\r\n"
-                    + "HIGHEST RX rate............. 100 MBPS\r\n"
-                    + "BIT RATE.................... 100000000\r\n"
-                    + "WORD LEN.................... 16\r\n"
-                    + "WORD/FRAME.................. 511\r\n"
-                    + "BITS/FRAME.................. 8192\r\n"
-                    + "MODULATION TYPE............. PCM/FM\r\n"
-                    + "TX Digital Los.............. 0.75\r\n"
-                    + "RX Digital Los.............. 0.10\r\n"
-                    + "BEP Test.................... -5\r\n"
-                    + "Local Rtrn Count............ 00\r\n"
-                    + "Remote Rtrn Count........... 00";
+                            + "HIGHEST TX rate............. 100 MBPS\r\n"
+                            + "LAST RX rate................ 100 MBPS\r\n"
+                            + "HIGHEST RX rate............. 100 MBPS\r\n"
+                            + "BIT RATE.................... 100000000\r\n"
+                            + "WORD LEN.................... 16\r\n"
+                            + "WORD/FRAME.................. 511\r\n"
+                            + "BITS/FRAME.................. 8192\r\n"
+                            + "MODULATION TYPE............. PCM/FM\r\n"
+                            + "TX Digital Los.............. 0.75\r\n"
+                            + "RX Digital Los.............. 0.10\r\n"
+                            + "BEP Test.................... -5\r\n"
+                            + "Local Rtrn Count............ 00\r\n"
+                            + "Remote Rtrn Count........... 00";
 
         }
 
@@ -57,22 +57,24 @@ public class TelemetryClient {
     }
 
     public String receive() {
-        StringBuilder message;
-
         if (StringUtils.isEmpty(diagnosticMessageResult)) {
-            // simulate a received message (just for illustration - not needed for this exercise)
-            message = new StringBuilder();
-            int messageLength = connectionEventsSimulator.nextInt(50) + 60;
-            for (int i = messageLength; i >= 0; --i) {
-                message.append((char) connectionEventsSimulator.nextInt(40) + 86);
-            }
-
-        } else {
-            message = new StringBuilder(diagnosticMessageResult);
-            diagnosticMessageResult = "";
+            return simulateMessage().toString();
         }
 
-        return message.toString();
+        StringBuilder resultMessage = new StringBuilder(diagnosticMessageResult);
+        diagnosticMessageResult = "";
+
+        return resultMessage.toString();
+    }
+
+    private StringBuilder simulateMessage() {
+        // simulate a received message (just for illustration - not needed for this exercise)
+        StringBuilder message = new StringBuilder();
+        int messageLength = connectionEventsSimulator.nextInt(50) + 60;
+        for (int i = messageLength; i >= 0; --i) {
+            message.append((char) connectionEventsSimulator.nextInt(40) + 86);
+        }
+        return message;
     }
 }
 
