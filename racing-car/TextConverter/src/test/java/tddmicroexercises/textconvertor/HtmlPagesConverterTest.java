6b075d5057f2b06a8c3c818fa5abf6bb58b949cc
Cloneable <1573652017@qq.com>
2020-04-15 22:10:36 +0800

    add convert html page test

--- racing-car/TextConverter/src/test/java/tddmicroexercises/textconvertor/HtmlPagesConverterTest.java
+++ racing-car/TextConverter/src/test/java/tddmicroexercises/textconvertor/HtmlPagesConverterTest.java
@@ -1,16 +1,33 @@
 package tddmicroexercises.textconvertor;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import org.junit.Test;
 
 
 public class HtmlPagesConverterTest {
+    public static final String PATH_PREFIX = "src/test/resources/";
+    public static final String SOURCE_FILE = "text_page.txt";
+
     @Test
     public void foo() throws IOException {
         HtmlPagesConverter converter = new HtmlPagesConverter("foo");
         assertEquals("foo", converter.getFilename());
+    }
+
+    @Test
+    public void should_get_page_html_when_text_with_page_break() throws IOException {
+        HtmlPagesConverter converter = new HtmlPagesConverter(PATH_PREFIX + SOURCE_FILE);
+
+        byte[] buff = Files.readAllBytes(Paths.get(PATH_PREFIX + "html_page_convert_result.html"));
+        String expectedString = new String(buff, Charset.defaultCharset());
+
+        assertThat(converter.getHtmlPage(1), is(expectedString));
     }
 }
