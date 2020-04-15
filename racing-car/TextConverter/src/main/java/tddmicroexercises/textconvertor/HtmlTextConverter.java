5dd8bd89382c1426f3e94c6bebdf3d476c102612
Cloneable <1573652017@qq.com>
2020-04-15 21:52:19 +0800

    format HtmlTextConverter

--- racing-car/TextConverter/src/main/java/tddmicroexercises/textconvertor/HtmlTextConverter.java
+++ racing-car/TextConverter/src/main/java/tddmicroexercises/textconvertor/HtmlTextConverter.java
@@ -4,32 +4,29 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class HtmlTextConverter
-{
+public class HtmlTextConverter {
     private String fullFilenameWithPath;
 
-    public HtmlTextConverter(String fullFilenameWithPath)
-    {
+    public HtmlTextConverter(String fullFilenameWithPath) {
         this.fullFilenameWithPath = fullFilenameWithPath;
     }
 
-    public String convertToHtml() throws IOException{
-    
-	    BufferedReader reader = new BufferedReader(new FileReader(fullFilenameWithPath));
-	    
-	    String line = reader.readLine();
-	    StringBuilder html = new StringBuilder();
-	    while (line != null)
-	    {
-	    	html.append(StringEscapeUtils.escapeHtml(line));
-	        html.append("<br />");
-	        line = reader.readLine();
-	    }
-	    return html.toString();
+    public String convertToHtml() throws IOException {
+
+        BufferedReader reader = new BufferedReader(new FileReader(fullFilenameWithPath));
+
+        String line = reader.readLine();
+        StringBuilder html = new StringBuilder();
+        while (line != null) {
+            html.append(StringEscapeUtils.escapeHtml(line));
+            html.append("<br />");
+            line = reader.readLine();
+        }
+        return html.toString();
 
     }
 
-	public String getFilename() {
-		return this.fullFilenameWithPath;
-	}
+    public String getFilename() {
+        return this.fullFilenameWithPath;
+    }
 }
