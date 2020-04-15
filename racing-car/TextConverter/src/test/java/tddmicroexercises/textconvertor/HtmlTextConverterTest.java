3ab645b0efd38dd129d022b7468243e4eca61649
Cloneable <1573652017@qq.com>
2020-04-15 21:50:03 +0800

    add convert text to html test

--- racing-car/TextConverter/src/test/java/tddmicroexercises/textconvertor/HtmlTextConverterTest.java
+++ racing-car/TextConverter/src/test/java/tddmicroexercises/textconvertor/HtmlTextConverterTest.java
@@ -1,13 +1,33 @@
 package tddmicroexercises.textconvertor;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class HtmlTextConverterTest {
+
+    public static final String PATH_PREFIX = "src/test/resources/";
+    public static final String SOURCE_FILE = "text.txt";
+
     @Test
-    public void foo() {
-        HtmlTextConverter converter = new HtmlTextConverter("foo");
-        assertEquals("foo", converter.getFilename());
+    public void should_get_file_name_when_initialize_html_file_path() {
+        HtmlTextConverter converter = new HtmlTextConverter(SOURCE_FILE);
+        assertEquals(SOURCE_FILE, converter.getFilename());
+    }
+
+    @Test
+    public void should_get_html_string_when_convert_text_to_html() throws IOException {
+        HtmlTextConverter converter = new HtmlTextConverter(PATH_PREFIX + SOURCE_FILE);
+
+        byte[] buff = Files.readAllBytes(Paths.get(PATH_PREFIX + "html_text_convert_result.html"));
+        String expectedString = new String(buff, Charset.defaultCharset());
+
+        assertThat(converter.convertToHtml(), is(expectedString));
     }
 }
