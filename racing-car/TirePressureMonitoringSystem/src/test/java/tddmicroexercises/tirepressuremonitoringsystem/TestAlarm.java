0715bbd3c462f97f1c9e807bbd7bfe12d8ba4c08
Cloneable <1573652017@qq.com>
2020-04-17 20:05:12 +0800

    rename should_not_alarm_on_when_initialize_alarm

--- racing-car/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/TestAlarm.java
+++ racing-car/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/TestAlarm.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 public class TestAlarm {
 
     @Test
-    public void foo() {
+    public void should_not_alarm_on_when_initialize_alarm() {
         Alarm alarm = new Alarm();
         assertFalse(alarm.isAlarmOn());
     }
