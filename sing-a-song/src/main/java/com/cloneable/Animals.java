8d17eb53290befc717d437a7186940cefce96948
Cloneable <1573652017@qq.com>
2020-04-25 22:08:54 +0800

    change parameter of Animals.catchAnimal

--- sing-a-song/src/main/java/com/cloneable/Animals.java
+++ sing-a-song/src/main/java/com/cloneable/Animals.java
@@ -9,20 +9,21 @@ public class Animals {
         this.animals = animals;
     }
 
-    private String catchAnimal(String previousAnimal, String animal) {
-        return "She swallowed the " + animal + " to catch the " + previousAnimal;
+    private String catchAnimal(Animal previousAnimal, Animal animal) {
+        return "She swallowed the " + animal.getName() + " to catch the " + previousAnimal.getName();
     }
 
     public String catchAnimals(int animalIndex) {
-        Animal[] caughtAnimals = Arrays.copyOf(this.animals, animalIndex + 1);
         StringBuilder sb = new StringBuilder();
         sb.append(animals[animalIndex].swallowed());
+
+        Animal[] caughtAnimals = Arrays.copyOf(this.animals, animalIndex + 1);
         for (int i = caughtAnimals.length - 1; i > 1; i--) {
-            sb.append(catchAnimal(caughtAnimals[i - 1].getName(), caughtAnimals[i].getName()))
+            sb.append(catchAnimal(caughtAnimals[i - 1], caughtAnimals[i]))
                     .append(",\n");
         }
 
-        sb.append(catchAnimal(caughtAnimals[0].getName(), caughtAnimals[1].getName()))
+        sb.append(catchAnimal(caughtAnimals[0], caughtAnimals[1]))
                 .append(";\n");
         return sb.toString();
     }
